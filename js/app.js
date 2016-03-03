97ac4b70fa025638263fa63942e191661b42ec8d
Maciek G <mgorczynski@handsonsystems.co.uk>
2016-03-03 11:44:29 +0100

    angularjs, boostrap and ctrl in place

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var wotblitzApp = angular.module('wotblitzApp', []);
+
+wotblitzApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
+    console.log("Hello World from controller");
+}]);
